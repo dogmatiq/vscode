c59f939e94a1047832e454d449a6dcf07d76b833
James Harris <contact@jamesharris.id.au>
2023-04-23 10:37:16 +1000

    Invoke language server.

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,41 @@
-import * as vscode from 'vscode';
+import {
+	workspace,
+	ExtensionContext,
+ } from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	TransportKind,
+ } from 'vscode-languageclient/node';
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "dogma" is now active!');
+let client: LanguageClient;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('dogma.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Dogma 2!');
-	});
+export function activate(context: ExtensionContext) {
+	client = new LanguageClient(
+		'dogmatiq/dogma',
+		'Dogma Language Server',
+		{
+			command: "dogma",
+			args: ["lsp"],
+		},
+		{
+			documentSelector: [
+				{
+					scheme: 'file',
+					language: 'go',
+				},
+			],
+		},
+	);
 
-	context.subscriptions.push(disposable);
+	client.start();
 }
 
-export function deactivate() {}
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+
+	return client.stop();
+}
