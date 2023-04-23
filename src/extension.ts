e3904a58aa95b57e8a0c8176161a56536a2f2b12
James Harris <contact@jamesharris.id.au>
2023-04-23 10:20:50 +1000

    Setup launcher.

--- src/extension.ts
+++ src/extension.ts
@@ -12,7 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('dogma.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Dogma!');
+		vscode.window.showInformationMessage('Hello World from Dogma 2!');
 	});
 
 	context.subscriptions.push(disposable);
