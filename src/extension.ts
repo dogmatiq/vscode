d10a9aa472efc38f467bf166afa84b3c07a71a86
James Harris <contact@jamesharris.id.au>
2023-04-26 06:38:52 +1000

    Add `dogma.restartLanguageServer` command.

--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,41 @@
-import {
-	workspace,
-	ExtensionContext,
- } from 'vscode';
-
-import {
-	LanguageClient,
-	LanguageClientOptions,
-	TransportKind,
- } from 'vscode-languageclient/node';
+import { commands, ExtensionContext } from "vscode";
+import { LanguageClient, TransportKind } from "vscode-languageclient/node";
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	client = new LanguageClient(
-		'dogmatiq/dogma',
-		'Dogma Language Server',
-		{
-			command: "dogma",
-			args: ["lsp"],
-		},
-		{
-			documentSelector: [
-				{
-					scheme: 'file',
-					language: 'go',
-				},
-			],
-		},
-	);
+  context.subscriptions.push(
+    commands.registerCommand("dogma.restartLanguageServer", async () => {
+      if (client) {
+        return client.restart();
+      }
+    })
+  );
 
-	client.start();
+  client = new LanguageClient(
+    "dogmatiq/dogma",
+    "Dogma Language Server",
+    {
+      command:
+        "/Users/james/grit/github.com/dogmatiq/dogmacli/artifacts/build/debug/darwin/arm64/dogma",
+      args: ["lsp"],
+      transport: TransportKind.stdio,
+    },
+    {
+      documentSelector: [
+        {
+          scheme: "file",
+          language: "go",
+        },
+      ],
+    }
+  );
+
+  client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-
-	return client.stop();
+  if (client) {
+    return client.stop();
+  }
 }
